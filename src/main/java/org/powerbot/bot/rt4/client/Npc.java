67951ab25b861362204012c0b39e4113999ade8b
Rick van Biljouw <rick@bipush.uk>
2020-10-24 18:37:17 +0200

    fixed a NPE in the npc wrapper

--- src/main/java/org/powerbot/bot/rt4/client/Npc.java
+++ src/main/java/org/powerbot/bot/rt4/client/Npc.java
@@ -1,6 +1,5 @@
 package org.powerbot.bot.rt4.client;
 
-import org.powerbot.bot.*;
 import org.powerbot.bot.rt4.client.internal.INpc;
 
 public class Npc extends Actor<INpc> {
@@ -13,7 +12,6 @@ public class Npc extends Actor<INpc> {
 		if (!isNull()) {
 			return new NpcConfig(wrapped.get().getConfig());
 		}
-
-		return null;
+		return new NpcConfig(null);
 	}
 }
