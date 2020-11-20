a92a0d28c403d09a93314f9a692988d28a672d86
Rick van Biljouw <rick@bipush.uk>
2020-11-20 16:14:55 +0100

    fixed some mouse bugs, renamed a menu entry

--- src/main/java/org/powerbot/script/rt4/Interactive.java
+++ src/main/java/org/powerbot/script/rt4/Interactive.java
@@ -82,7 +82,7 @@ public abstract class Interactive extends ClientAccessor implements org.powerbot
 	 */
 	@Override
 	public final boolean click(final boolean left) {
-		return click(MouseEvent.BUTTON1);
+		return click(left ? MouseEvent.BUTTON1 : MouseEvent.BUTTON2);
 	}
 
 	/**
