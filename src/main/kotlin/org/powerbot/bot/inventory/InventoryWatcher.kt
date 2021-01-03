a030f5be11909d83fa2fe9f951224f0848617a48
constt <ijavaservices@gmail.com>
2021-01-03 15:24:01 +1300

    add annotations to inv watcher

--- src/main/kotlin/org/powerbot/bot/inventory/InventoryWatcher.kt
+++ src/main/kotlin/org/powerbot/bot/inventory/InventoryWatcher.kt
@@ -1,12 +1,10 @@
 package org.powerbot.bot.inventory
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
 import org.powerbot.bot.rt4.client.ItemConfig
 import org.powerbot.script.*
 import org.powerbot.script.rt4.CacheItemConfig
@@ -16,13 +14,16 @@ import java.util.concurrent.atomic.AtomicBoolean
 import java.util.logging.Level
 import java.util.logging.Logger
 
+@FlowPreview
 @InternalCoroutinesApi
 class InventoryWatcher : GameActionListener, MessageListener, GameTickListener {
 
     val logger = Logger.getLogger(javaClass.name)
-    var initialized: AtomicBoolean = AtomicBoolean(false)
+    var initialized: Boolean = false
+    var loginTime = 0L
     var cache = mapOf<Int, Int>()
 
+    @ExperimentalCoroutinesApi
     private val channel = BroadcastChannel<Boolean>(1)
 
     init {
@@ -31,23 +32,26 @@ class InventoryWatcher : GameActionListener, MessageListener, GameTickListener {
         }
     }
 
+    @FlowPreview
     suspend fun observe() {
         val check = {
             try {
                 if (!ClientContext.ctx().game.loggedIn() || ClientContext.ctx().client().clientState != 30) {
-                    initialized.set(false)
+                    initialized = false
                 } else {
                     val items = ClientContext.ctx().inventory.items()
                         .filterNotNull().filter { it.id() > 0 }
                         .groupBy { it.id() }
                         .map { it.key to it.value.map { item -> item.stackSize() }.sum() }.toMap()
-                    if (!initialized.get()) {
-                        cache = items
-                        initialized.set(true)
+                    if (items.size > 0 || (loginTime > 0 && System.currentTimeMillis() - loginTime > 1500)) {
+                        if (!initialized) {
+                            cache = items
+                            initialized = true
+                        } else {
+                            diffMaps(items, cache)
+                            cache = items
+                        }
                     }
-
-                    diffMaps(items, cache)
-                    cache = items
                 }
             } catch (t: Throwable) {
                 logger.log(Level.WARNING, "Failed watching inventory", t)
@@ -65,12 +69,12 @@ class InventoryWatcher : GameActionListener, MessageListener, GameTickListener {
     private fun diffMaps(new: Map<Int, Int>, old: Map<Int, Int>) {
         new.forEach {
             if (!old.containsKey(it.key)) {
-                if (initialized.get())
+                if (initialized)
                     ClientContext.ctx().bot().dispatcher.dispatch(
                         InventoryChangeEvent(it.key, it.value, it.value, CacheItemConfig.load(ClientContext.ctx().bot().cacheWorker, it.key)?.name)
                     )
             } else if (old[it.key] != it.value) {
-                if (initialized.get())
+                if (initialized)
                     ClientContext.ctx().bot().dispatcher.dispatch(InventoryChangeEvent(
                         it.key,
                         it.value - (old[it.key] ?: 0),
@@ -81,7 +85,7 @@ class InventoryWatcher : GameActionListener, MessageListener, GameTickListener {
         }
         old.forEach {
             if (!new.containsKey(it.key)) {
-                if (initialized.get())
+                if (initialized)
                     ClientContext.ctx().bot().dispatcher.dispatch(
                         InventoryChangeEvent(it.key, it.value * -1, 0, CacheItemConfig.load(ClientContext.ctx().bot().cacheWorker, it.key)?.name)
                     )
@@ -89,20 +93,26 @@ class InventoryWatcher : GameActionListener, MessageListener, GameTickListener {
         }
     }
 
+    @ExperimentalCoroutinesApi
     override fun onAction(evt: GameActionEvent) {
         GlobalScope.launch {
             channel.send(true)
-            Condition.wait({
-                ClientContext.ctx().players.local().animation() == -1 &&
-                    !ClientContext.ctx().players.local().inMotion()
-            }, 300, 10)
+            val start = System.currentTimeMillis()
+            while (System.currentTimeMillis() - start < 5000 &&
+                (ClientContext.ctx().players.local().animation() == -1 &&
+                !ClientContext.ctx().players.local().inMotion())
+            ) {
+                delay(100)
+            }
             channel.send(true)
         }
     }
 
+    @ExperimentalCoroutinesApi
     override fun messaged(event: MessageEvent?) {
         if (event?.type() == 108 && event.text() == "Welcome to Old School RuneScape.") {
-            initialized.set(false)
+            loginTime = System.currentTimeMillis()
+            initialized = false
         }
 
         if (event?.type() != 2) {
@@ -112,6 +122,7 @@ class InventoryWatcher : GameActionListener, MessageListener, GameTickListener {
         }
     }
 
+    @ExperimentalCoroutinesApi
     override fun onGameTick(evt: GameTickEvent) {
         GlobalScope.launch {
             channel.send(true)
