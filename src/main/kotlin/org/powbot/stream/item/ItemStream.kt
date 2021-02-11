97984b9213539d9b5f41eee36b3aece340fdf2a1
constt <ijavaservices@gmail.com>
2021-02-11 18:19:34 +1300

    make item stream identifiable

--- src/main/kotlin/org/powbot/stream/item/ItemStream.kt
+++ src/main/kotlin/org/powbot/stream/item/ItemStream.kt
@@ -3,6 +3,7 @@ package org.powbot.stream.item
 import org.powbot.stream.locatable.interactive.InteractiveStream
 import org.powbot.stream.SimpleStream
 import org.powbot.stream.ops.ActionableOps
+import org.powbot.stream.ops.IdentifiableOps
 import org.powbot.stream.ops.NameableOps
 import org.powerbot.script.rt4.ClientContext
 import org.powerbot.script.rt4.Item
@@ -11,6 +12,7 @@ import java.util.stream.*
 abstract class ItemStream<T : Item, S : SimpleStream<T, S>>(ctx: ClientContext, stream: Stream<T>) :
     InteractiveStream<T, S>(ctx, stream),
     ActionableOps<T, S>,
+    IdentifiableOps<T, S>,
     NameableOps<T, S> {
 
     /**
