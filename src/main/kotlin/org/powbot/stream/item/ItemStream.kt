59811e26d501df0a12d88c8283a4f61c1d820eed
constt <ijavaservices@gmail.com>
2021-02-11 18:12:43 +1300

    add countStacks

--- src/main/kotlin/org/powbot/stream/item/ItemStream.kt
+++ src/main/kotlin/org/powbot/stream/item/ItemStream.kt
@@ -8,6 +8,16 @@ import org.powerbot.script.rt4.ClientContext
 import org.powerbot.script.rt4.Item
 import java.util.stream.*
 
-abstract class ItemStream<T: Item, S: SimpleStream<T, S>>(ctx: ClientContext, stream: Stream<T>) : InteractiveStream<T, S>(ctx, stream),
+abstract class ItemStream<T : Item, S : SimpleStream<T, S>>(ctx: ClientContext, stream: Stream<T>) :
+    InteractiveStream<T, S>(ctx, stream),
     ActionableOps<T, S>,
-    NameableOps<T, S>
+    NameableOps<T, S> {
+
+    fun count(countStacks: Boolean): Long {
+        if (!countStacks) {
+            return count()
+        }
+
+        return filter { it != null }.mapToLong { it.stackSize().toLong() }.sum()
+    }
+}
