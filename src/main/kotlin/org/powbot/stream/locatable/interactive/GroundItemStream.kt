07a6bfe482a600012eb28f7dc842aa3a7a1a44fe
constt <ijavaservices@gmail.com>
2021-03-17 09:19:42 +1300

    make ground item id filter

--- src/main/kotlin/org/powbot/stream/locatable/interactive/GroundItemStream.kt
+++ src/main/kotlin/org/powbot/stream/locatable/interactive/GroundItemStream.kt
@@ -1,6 +1,7 @@
 package org.powbot.stream.locatable.interactive
 
 import org.powbot.stream.ops.ActionableOps
+import org.powbot.stream.ops.IdentifiableOps
 import org.powbot.stream.ops.LocatableOps
 import org.powbot.stream.ops.NameableOps
 import org.powerbot.bot.rt4.client.internal.IItemNode
@@ -13,6 +14,7 @@ class GroundItemStream(ctx: ClientContext, stream: Stream<GroundItem>)
     : InteractiveStream<GroundItem, GroundItem, GroundItemStream>(ctx, stream, {  it }),
     ActionableOps<GroundItem, GroundItem, GroundItemStream>,
     LocatableOps<GroundItem, GroundItem, GroundItemStream>,
+    IdentifiableOps<GroundItem, GroundItem, GroundItemStream>,
     NameableOps<GroundItem, GroundItem, GroundItemStream> {
 
     override fun nil(): GroundItem {
