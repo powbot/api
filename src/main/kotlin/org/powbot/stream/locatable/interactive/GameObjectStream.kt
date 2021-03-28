c852ddfdb3d1bea7779d5ff04e50965403987985
constt <ijavaservices@gmail.com>
2021-03-28 20:44:29 +1300

    add id filters to game object stream

--- src/main/kotlin/org/powbot/stream/locatable/interactive/GameObjectStream.kt
+++ src/main/kotlin/org/powbot/stream/locatable/interactive/GameObjectStream.kt
@@ -1,6 +1,7 @@
 package org.powbot.stream.locatable.interactive
 
 import org.powbot.stream.ops.ActionableOps
+import org.powbot.stream.ops.IdentifiableOps
 import org.powbot.stream.ops.LocatableOps
 import org.powbot.stream.ops.NameableOps
 import org.powerbot.script.rt4.ClientContext
@@ -9,6 +10,7 @@ import java.util.stream.*
 
 class GameObjectStream(ctx: ClientContext, stream: Stream<GameObject>) : InteractiveStream<GameObject, GameObjectStream>(ctx, stream),
     ActionableOps<GameObject, GameObjectStream>,
+    IdentifiableOps<GameObject, GameObjectStream>,
     LocatableOps<GameObject, GameObjectStream>,
     NameableOps<GameObject, GameObjectStream> {
     override fun nil(): GameObject {
